3012222277f04a1c500261371fa1f8526783c837
Arisa Tajima <atajima@iastate.edu>
2016-03-23 23:28:39 -0500

    modified

--- db_helper.php
+++ db_helper.php
@@ -1,109 +1,58 @@
 <?php
-function db_get_hash($username) {
-    try{
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="select password from credentials where username=?";
-        $conn->prepare($query);
-        $stmt->bind_param("s", $username);
-        $stmt->bind_result($hash);
-        mysql_close();
-        return $hash;
-    }catch(Exception $e){
-        return 'idk';
+     
+    $M_host = 'localhost';
+    $M_user = '';
+    $M_password = '';
+    $M_database = '';
+    
+    global $mysqli;
+    
+    $mysqli = new mysqli($M_host, $M_user, $M_password, $M_database);
+    if ($mysqli->connect_errno) {
+        die("Failed to connect to MySQL: " . $mysqli->connect_error);
     }
-}
+    $mysqli->set_charset("utf8");
 
-function db_get_salt($username) {
-    try{
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="select salt from credentials where username=?";
-        $conn->prepare($query);
-        $stmt->bind_param("s", $username);
-        $stmt->bind_result($salt);
-        mysql_close();
-        return $salt;
-    }catch(Exception $e){
-        return 'NaCl';
+    unset($M_host);
+    unset($M_user);
+    unset($M_password);
+    unset($M_database);
+	
+function clean_input($regex, $input) {
+	if ($regex != "comments"){
+		$lstring = preg_replace($regex, '', str_replace(chr(0), '', $input));
+	}
+	else{
+		$lstring = preg_replace(str_replace(chr(0), '', $input));
+	}
+    $lstring = preg_replace($regex, '', str_replace(chr(0), '', $input));
+    $string = stripslashes(strip_tags($lstring));
+    while ($string != $lstring) {
+        $lstring = $string;
+        $string = stripslashes(strip_tags($lstring));
     }
-}
 
-function db_user_exists($username) {
-    $user = "web";
-    $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-    $db = "web";
-    $host = "localhost";
-    mysql_connect($host,$user,$pass);
-    mysql_select_db($db) or die("Unable to select database");
-    $query="select * from credentials where username=?";
-    $conn->prepare($query);
-    $stmt->bind_param("s", $username);
-    $stmt->bind_result($hash);
-
-    $result=mysql_query($query);
-    if (!$result) {
-        echo mysql_error();
-        return false;
-    }
-    mysql_close();
-    if(mysql_num_rows($result) > 0){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function db_get_group($username){
-    try{
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="select u_group from credentials where username=?";
-        $conn->prepare($query);
-        $stmt->bind_param("s", $username);
-        $stmt->bind_result($group);
-        mysql_close();
-        return $group;
-    }catch(Exception $e){
-        return 'admin';
-    }
+    return stripslashes($string);
 }
 
 function db_get_creditcard($username) {
-    $user = "web";
-    $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-    $db = "web";
-    $host = "localhost";
-    mysql_connect($host,$user,$pass);
-    mysql_select_db($db) or die("Unable to select database");
+ 
+    global $mysqli;
     $query="select creditcard from credentials where username=?";
-    $conn->prepare($query);
+    $stmt=$mysqli->prepare($query);
     $stmt->bind_param("s", $username);
-    $stmt->bind_result($cc);
-    $result=mysql_query($query);
-    mysql_close();
+    $cc = NULL;
+	$stmt->bind_result($cc);
+	if(!$stmt->fetch()){
+			$cc ="";
+	}
+    
     return $cc;
 }
 
 function db_get_users(){
-    $user = "web";
-    $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-    $db = "web";
-    $host = "localhost";
-    mysql_connect($host,$user,$pass);
-    mysql_select_db($db) or die("Unable to select database");
+   
+    global $mysqli;
     $query="select * from credentials";
     $users = array();
     $result=mysql_query($query);
@@ -114,111 +63,35 @@ function db_get_users(){
     return $users;
 }
 
-function db_create_user($username,$password,$salt,$creditcard,$group){
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="insert into credentials (username,password,salt,creditcard,u_group) values (?,?,?,?,?)";
-        $stmt->bind_param("sssss", $username, $password, $salt, creditcard, u_group) VALUES (?,?,?,?,?);
-        mysql_query($query);
-        mysql_close();
-        return '0';
+function db_update_user($username,$creditcard){
+        
+    global $mysqli;
+    $query="update credentials set username='?', creditcard='?' where username='?'";
+    $stmt=$mysqli->prepare($query);
+    $stmt->bind_param("sss", $username, $creditcard, $_SESSION['username']);
+	if(!$stmt->fetch()){
+			return false;
+	}
+    $_SESSION['username'] = $username;
+    return true;
 }
 
-function db_update_user($username,$password,$salt,$creditcard,$group){
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="update credentials set username='$username', password='$password', salt='$salt', creditcard='$creditcard', u_group='$group' where username='$username'";
-        mysql_query($query);
-        mysql_close();
-        return '0';
-}
-
-function db_update_cc($username,$creditcard,$group){
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="update credentials set username='$username', creditcard='$creditcard', u_group='$group' where username='$username'";
-        mysql_query($query);
-        mysql_close();
-        return '0';
-}
-
-function db_insert_cookie($username,$name,$value){
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="insert into cookies (username,name,value) values (?,?,?)";
-        $stmt->bind_param("sss", $username, $name, $value);
-        $stmt->exec();
-        mysql_query($query);
-        mysql_close();
-        return '0';
-}
-
-function db_get_cookie_user($value){
-    try{
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="select * from cookies where value='".$value."'";
-        $username = '0';
-        $result=mysql_query($query);
-        while ($row = mysql_fetch_assoc($result)) {
-            $username = $row["username"];
-        }
-        mysql_close();
-        return $username;
-    }catch(Exception $e){
-        return 'cdc';
-    }
-
-}
-
-function db_get_cookie_group($value) {
-    try {
-        $user = "web";
-        $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-        $db = "web";
-        $host = "localhost";
-        mysql_connect($host,$user,$pass);
-        mysql_select_db($db) or die("Unable to select database");
-        $query="select * from cookies where value='".$value."'";
-        $group = '';
-        $result=mysql_query($query);
-        while ($row = mysql_fetch_assoc($result)) {
-            $group = $row["u_group"];
-        }
-        mysql_close();
-        return $group;
-    }catch(Exception $e){
-        return 'admin';
-    }
+function db_update_cc($username,$creditcard){
+       
+    global $mysqli;
+    $query="update credentials set creditcard='?' where username='?'";
+	$stmt=$mysqli->prepare($query);
+    $stmt->bind_param("ss", $creditcard, $username);
+	if(!$stmt->fetch()){
+			return false;
+	}
+   
+    return true;
 }
 
 function db_get_comments($num) {
-    $user = "web";
-    $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-    $db = "web";
-    $host = "localhost";
-    mysql_connect($host,$user,$pass);
-    mysql_select_db($db) or die("Unable to select database");
+   // TODO: Sanitize output
+    global $mysqli;
     $query="select * from comments limit ".$num;
     $stmt = $conn->prepare()
     $result=mysql_query($query);
@@ -227,12 +100,9 @@ function db_get_comments($num) {
 }
 
 function db_put_comment($comment) {
-    $user = "web";
-    $pass = "7f0DlNcl3Lv5rQ6sHOuq";
-    $db = "web";
-    $host = "localhost";
-    mysql_connect($host,$user,$pass);
-    mysql_select_db($db) or die("Unable to select database");
+    // TODO: Sanitize input
+	global $mysqli;
+
     $sql = "INSERT INTO comments (c) VALUES (?)";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("s", $comment);
